1b6e1ddca2bb6ae5b498b7ba5433fbd208caed51
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-03-23 23:04:50 +0600

    section1 working

--- src/Pages/DebustLanding/DebustLanding.js
+++ src/Pages/DebustLanding/DebustLanding.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import './DebustLanding.css';
+import React from "react";
+import "./DebustLanding.css";
 function DebustLanding(props) {
-    return (
-        <div id='DebustLanding'>
-            <section id='Section1'>
-                <div className="section_container">
+  return (
+    <div id="DebustLanding">
+      <section id="Section1">
+        <div className="section_container">
+          <div className="d-flex flex_wrapper">
+            <div className="box box1">
+              <h1 className="mb_head">Democratizing Experiences in Web 3.0</h1>
+              <h5 className="mb_head">
+                Write Reviews. Earn Debust Coins. Make informed decisions with{" "}
+                <br />
+                authentic decentralized recommendations.
+              </h5>
+            </div>
+            <div className="box"></div>
+          </div>
 
-                </div>
-            </section>
+          <div className="btn_wrapper d-flex flex-wrap">
+            <a href="#" className="btn_explore_outline mr_15">
+              Explore Now
+            </a>
+
+            <a href="#" className="btn_creator">
+              Get Started
+            </a>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 }
 
 export default DebustLanding;
